8abcade3111dbe5981e2fc55b441f15e47906c50
Matěj Kripner <kripnermatej@gmail.com>
2018-05-15 21:48:27 +0200

    Refactor Problem layout to use Table

--- lib/problem.dart
+++ lib/problem.dart
@@ -70,13 +70,13 @@ class _ProblemState extends State<Problem> {
 
   void _numberSubmitted(String value) {
     if (value.isEmpty) return;
-    widget.taskData.lastSubmittedInput = value;
     int answer;
     try {
       answer = int.parse(value);
     } on FormatException {
       return;
     }
+    widget.taskData.lastSubmittedInput = value;
     bool isCorrect = widget.taskData.isCorrect(answer);
 
     if (isCorrect) {
@@ -91,8 +91,7 @@ class _ProblemState extends State<Problem> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget.taskData.status != TaskStatus.SUCCESS)
-      FocusScope.of(context).requestFocus(_inputFocusNode);
+    if (widget.taskData.status != TaskStatus.SUCCESS) FocusScope.of(context).requestFocus(_inputFocusNode);
     TextTheme textTheme = Theme.of(context).textTheme;
 
     return new Padding(
@@ -106,14 +105,7 @@ class _ProblemState extends State<Problem> {
             flex: 1,
             child: new Container(),
           ),
-          new Center(child: _buildExpression(textTheme.display1.copyWith(fontSize: 35.0))),
-          new Padding(
-            child: new Text(
-              'Difficulty ' + widget.taskData.difficulty.toString(),
-              style: Theme.of(context).textTheme.body2.copyWith(fontSize: 18.0),
-            ),
-            padding: const EdgeInsets.fromLTRB(20.0, 7.0, 0.0, 0.0),
-          ),
+          _buildLayoutTable(),
           new Flexible(
             flex: 3,
             child: new Container(),
@@ -123,43 +115,88 @@ class _ProblemState extends State<Problem> {
     );
   }
 
-  Row _buildExpression(TextStyle taskStyle) {
-    return new Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        new Flexible(
-          child: widget.taskData.buildExpression().createExpression(style: taskStyle),
+  Widget _buildLayoutTable() {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    TextStyle taskStyle = textTheme.display1.copyWith(fontSize: 35.0);
+    return new Table(
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: <TableRow>[
+        new TableRow(
+          children: <Widget>[
+            _buildQuestionRow(taskStyle),
+            _buildAnswer(taskStyle),
+          ],
         ),
+        new TableRow(
+          children: <Widget>[
+            new Padding(
+              child: new Text(
+                'Difficulty ' + widget.taskData.difficulty.toString(),
+                style: Theme.of(context).textTheme.body2.copyWith(fontSize: 18.0),
+              ),
+              padding: const EdgeInsets.fromLTRB(20.0, 7.0, 0.0, 0.0),
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(right: 30.0),
+              child: _buildMistakeDisplay(textTheme.caption.copyWith(fontSize: 18.0)),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildQuestionRow(TextStyle taskStyle) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        widget.taskData.buildExpression().createExpression(style: taskStyle),
         new Padding(
           padding: new EdgeInsets.symmetric(horizontal: 5.0),
           child: new Text('=', style: taskStyle),
         ),
-        new Flexible(
-          child: new Stack(
-            alignment: AlignmentDirectional.centerStart,
-            children: <Widget>[
-              new Opacity(
-                  opacity: widget.taskData.answerShown ? 1.0 : 0.0,
-                  child: new Text(widget.taskData.getAnswer().toString(), style: taskStyle)),
-              new Opacity(
-                opacity: widget.taskData.answerShown ? 0.0 : 1.0,
-                child: new TextField(
-                  autocorrect: false,
-                  keyboardType: TextInputType.number,
-                  onSubmitted: _numberSubmitted,
-                  style: taskStyle,
-                  focusNode: _inputFocusNode,
-                  controller: _inputController,
-                  onChanged: (s) {
-                    _resetInfo();
-                    widget.taskData.userInput = s;
-                  },
-                ),
-              ),
-            ],
+      ],
+    );
+  }
+
+  Widget _buildAnswer(TextStyle taskStyle) {
+    return new Stack(
+      alignment: AlignmentDirectional.centerStart,
+      children: <Widget>[
+        new Opacity(
+            opacity: widget.taskData.answerShown ? 1.0 : 0.0,
+            child: new Text(widget.taskData.getAnswer().toString(), style: taskStyle)),
+        new Opacity(
+          opacity: widget.taskData.answerShown ? 0.0 : 1.0,
+          child: new TextField(
+            autocorrect: false,
+            keyboardType: TextInputType.number,
+            onSubmitted: _numberSubmitted,
+            style: taskStyle,
+            focusNode: _inputFocusNode,
+            controller: _inputController,
+            onChanged: (s) {
+              _resetInfo();
+              widget.taskData.userInput = s;
+            },
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildMistakeDisplay(TextStyle style) {
+    if (widget.taskData.status != TaskStatus.SHOWED_ANSWER || widget.taskData.lastSubmittedInput.isEmpty)
+      return new Opacity(
+        child: new Text('Calculating ...'),
+        opacity: 0.0,
+      );
+    return new Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: new Text(
+        widget.taskData.lastSubmittedInput,
+        style: style.copyWith(color: Colors.red, decoration: TextDecoration.lineThrough, fontStyle: FontStyle.italic),
+      ),
     );
   }
 
@@ -187,7 +224,11 @@ class _ProblemState extends State<Problem> {
           ],
         );
       case TaskStatus.SUCCESS:
-        return new Text(widget.successMessage, style: style, textAlign: TextAlign.center,);
+        return new Text(
+          widget.successMessage,
+          style: style,
+          textAlign: TextAlign.center,
+        );
       default:
         throw new Exception();
     }
